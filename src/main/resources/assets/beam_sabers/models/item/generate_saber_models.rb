a03232eb609b62cf69121ff02b7e00b33031228c
Lemon-Juiced <47193272+Lemon-Juiced@users.noreply.github.com>
2025-09-23 21:24:41 -0400

    Textures

--- src/main/resources/assets/beam_sabers/models/item/generate_saber_models.rb
+++ src/main/resources/assets/beam_sabers/models/item/generate_saber_models.rb
@@ -5,39 +5,67 @@ colors = %w[
 
 saber_types = [
   {
-    prefix: '', # standard
+    type: 'standard',
     hilt: 'standard_hilt',
-    blade: 'standard_%{color}_blade',
+    blade: '%{color}_standard_blade',
     name: '%{color}_beam_saber'
   },
   {
-    prefix: 'great_',
+    type: 'great',
     hilt: 'great_hilt',
-    blade: 'great_%{color}_blade',
+    blade: '%{color}_great_blade',
     name: '%{color}_great_beam_saber'
   },
   {
-    prefix: 'curved_hilt_',
+    type: 'curved_hilt',
     hilt: 'curved_hilt_hilt',
-    blade: 'curved_hilt_%{color}_blade',
+    blade: '%{color}_curved_hilt_blade',
     name: '%{color}_curved_hilt_beam_saber'
   }
 ]
+
+curved_hilt_display = <<~JSON.chomp
+  "display": {
+    "thirdperson_righthand": {
+      "rotation": [0, 90, -35],
+      "translation": [0, 4.0, 2.0],
+      "scale": [0.85, 0.85, 0.85]
+    },
+    "thirdperson_lefthand": {
+      "rotation": [0, 90, -35],
+      "translation": [0, 4.0, 2.0],
+      "scale": [0.85, 0.85, 0.85]
+    }
+  }
+JSON
 
 colors.each do |color|
   saber_types.each do |type|
     name = type[:name] % { color: color }
     hilt_texture = "beam_sabers:item/#{type[:hilt]}"
     blade_texture = "beam_sabers:item/#{type[:blade] % { color: color }}"
-    json_content = <<~JSON
-    {
-      "parent": "item/handheld",
-      "textures": {
-        "layer0": "#{hilt_texture}",
-        "layer1": "#{blade_texture}"
+    if type[:type] == 'curved_hilt'
+      json_content = <<~JSON
+      {
+        "parent": "item/handheld",
+        "textures": {
+          "layer0": "#{hilt_texture}",
+          "layer1": "#{blade_texture}"
+        },
+        #{curved_hilt_display}
       }
-    }
-    JSON
+      JSON
+    else
+      json_content = <<~JSON
+      {
+        "parent": "item/handheld",
+        "textures": {
+          "layer0": "#{hilt_texture}",
+          "layer1": "#{blade_texture}"
+        }
+      }
+      JSON
+    end
     File.write("#{name}.json", json_content)
   end
 end
